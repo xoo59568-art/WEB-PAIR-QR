bd655725ac6df12d4a57e5fdaa2191cf647c63c2
xoo59568-art <xoo59568@gmail.com>
2026-06-19 19:45:29 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import { Storage } from 'megajs';
 
 const auth = {
-    email: process.env.MEGA_EMAIL || 'Your mega email',
-    password: process.env.MEGA_PASSWORD || 'your mega password',
+    email: process.env.MEGA_EMAIL || 'oyysreejan8@gmail.com',
+    password: process.env.MEGA_PASSWORD || 'sreejan900',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
